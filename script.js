1ec36bddd44738f5b3d38ae8737ad258326e1f83
Jpdsf <jpdsafg@gmail.com>
2023-02-07 20:47:19 -0300

    Novo teste com camera

--- script.js
+++ script.js
@@ -1,43 +1,96 @@
-var video = document.querySelector('#video');
+feather.replace();
 
-var getUserMedia = (navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                          navigator.msGetUserMedia).bind(navigator);
+const controls = document.querySelector('.controls');
+const cameraOptions = document.querySelector('.video-options>select');
+const video = document.querySelector('video');
+const canvas = document.querySelector('canvas');
+const screenshotImage = document.querySelector('img');
+const buttons = [...controls.querySelectorAll('button')];
+let streamStarted = false;
 
-var mediaStream;
-function iniciaVideo (stream) {
-  video.srcObject = stream;
-  mediaStream = stream;
-}
+const [play, pause, screenshot] = buttons;
 
-function trataErroMedia (erro) {
-  console.error('Erro: ' + erro);
-}
-
-var configuracaoMedia = {video: {optional: [{maxWidth: 320},{maxHeight: 240}]}, audio: false};
-
-getUserMedia(configuracaoMedia, iniciaVideo, trataErroMedia);
-
-var canvas = document.querySelector('#canvas');
-canvas.width = 320;
-canvas.height = 240;
-
-var botaoTiraFoto = document.querySelector('#tira-foto');
-botaoTiraFoto.addEventListener('click', function (e) {
-  if (mediaStream) {
-    canvas.getContext('2d').drawImage(video, 0, 0, 320, 240);
-    var dados = canvas.toDataURL('image/png');
-    //fazer algo com os dados...
-    mediaStream.getVideoTracks().forEach(function (media) {
-      media.stop();
-    });
-    video.style.display = 'none';
-    canvas.style.display = '';
-    mediaStream = null;
-  } else {
-    getUserMedia(configuracaoMedia, iniciaVideo, trataErroMedia);
-    video.style.display = '';
-    canvas.style.display = 'none';
+const constraints = {
+  video: {
+    width: {
+      min: 1280,
+      ideal: 1920,
+      max: 2560,
+    },
+    height: {
+      min: 720,
+      ideal: 1080,
+      max: 1440
+    },
   }
-});
+};
 
+const getCameraSelection = async () => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(device => device.kind === 'videoinput');
+  const options = videoDevices.map(videoDevice => {
+    return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+  });
+  cameraOptions.innerHTML = options.join('');
+};
+
+play.onclick = () => {
+  if (streamStarted) {
+    video.play();
+    play.classList.add('d-none');
+    pause.classList.remove('d-none');
+    return;
+  }
+  if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
+    const updatedConstraints = {
+      ...constraints,
+      deviceId: {
+        exact: cameraOptions.value
+      }
+    };
+    startStream(updatedConstraints);
+  }
+};
+
+const startStream = async (constraints) => {
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+  handleStream(stream);
+};
+
+const handleStream = (stream) => {
+  video.srcObject = stream;
+  play.classList.add('d-none');
+  pause.classList.remove('d-none');
+  screenshot.classList.remove('d-none');
+  streamStarted = true;
+};
+
+getCameraSelection();
+
+
+cameraOptions.onchange = () => {
+  const updatedConstraints = {
+    ...constraints,
+    deviceId: {
+      exact: cameraOptions.value
+    }
+  };
+  startStream(updatedConstraints);
+};
+
+const pauseStream = () => {
+  video.pause();
+  play.classList.remove('d-none');
+  pause.classList.add('d-none');
+};
+
+const doScreenshot = () => {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').drawImage(video, 0, 0);
+  screenshotImage.src = canvas.toDataURL('image/webp');
+  screenshotImage.classList.remove('d-none');
+};
+
+pause.onclick = pauseStream;
+screenshot.onclick = doScreenshot;
